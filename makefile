48aff9d6ee1520e3421713538d3e54dca94329ca
cyber <aziz.sabyrbaev@bk.ru>
2024-03-28 20:06:13 +0300

    Fixing some problems with generating go and gRPC file

--- makefile
+++ makefile
@@ -1,3 +1,4 @@
 build:
 	protoc --proto_path=rocket/v1 --go_out=rocket/v1 --go-grpc_out=rocket/v1 rocket/v1/rocket.proto
 
+
--- rocket/v1/rocket_grpc.pb.go
+++ rocket/v1/rocket_grpc.pb.go
@@ -4,7 +4,7 @@
 // - protoc             v5.26.0
 // source: rocket.proto
 
-package __
+package v1
 
 import (
 	context "context"
